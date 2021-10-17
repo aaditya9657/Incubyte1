d858fbcec26aa699b33aef25c20deebadbc7003b
aaditya9657 <90003642+aaditya9657@users.noreply.github.com>
2021-10-18 01:35:32 +0530

    Test Case-5

--- string-calculator-starter-master (1)/string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
+++ string-calculator-starter-master (1)/string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
@@ -9,6 +9,7 @@ import java.util.TreeSet;
 class StringCalculator {
 
   
+
 public int add(String input) 
   {
 	  	int i=0;
@@ -18,6 +19,7 @@ public int add(String input)
 	  	TreeSet<Character> del = new TreeSet();
 	  	del.add('\n');
 	  	del.add(',');
+	  	ArrayList<Integer> negatives = new ArrayList();
 	  	
 	  	if(length>1 && input.charAt(0)=='/' && input.charAt(1)=='/')
 	  	{
@@ -45,9 +47,16 @@ public int add(String input)
 		  	  		{
 		  	  			a = 0;
 		  	  		}
-		  	  		sum = sum + a;
-		  	  		temp = "";
+		  	  		if(a<0)
+		  	  		{
+		  	  			negatives.add(a);
+		  	  		}
+		  	  		else
+		  	  		{
+			  	  		sum = sum + a;	
+		  	  		}
 		  	  		cont = true;
+					temp = "";
 		  		}
 		  		if(!cont)
 		  		{
@@ -65,8 +74,44 @@ public int add(String input)
 		{
 			a = 0;
 		}
-		sum = sum + a;
+		if(a<0)
+	  	{
+	  		negatives.add(a);
+	  	}
+	  	else
+	  	{
+	  		sum = sum + a;  	  		
+	  	}
 		temp = "";
+		int n_neg = negatives.size();
+		if(n_neg>0)
+		{
+			class MyException extends Exception
+			{
+				String exc = "";
+				public MyException(String exc)
+				{
+					super(exc);
+					this.exc = exc;
+				}
+				
+				@Override
+				public String toString()
+				{
+					return "negatives not allowed "+exc;
+				}
+			}
+			try
+			{
+				throw new MyException(Arrays.toString(negatives.toArray()));
+			}
+			catch(MyException e)
+			{
+				System.out.println(e);
+			}
+			
+		}
+		
 	  	
 	  	System.out.println(input+"  "+sum);
 	    return sum;
